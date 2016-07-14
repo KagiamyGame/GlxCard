f1ffee08acc5016ff60d001c0d86f59bb9f76743
chaunhan <justinngo159753@gmail.com>
2016-07-14 21:47:24 +0700

    Update NapVipCommand.php

--- src/Napthe/CMD/NapVipCommand.php
+++ src/Napthe/CMD/NapVipCommand.php
@@ -10,40 +10,5 @@ use napthe\BaokimTrans;
 use pocketmine\utils\TextFormat;
 		
 class NapVipCommand extends PluginBase implements Listener{
-	public function __construct(main $main, $name) {
- 		parent::__construct(
- 				$name, $main
- 		);
- 		$this->main = $main;
- 		$this->setPermission("napvip.command");
- 	}
- 
- 	public function Execute(CommandSender $sender, $currentAlias, array $args) {
- 	
- 		if (!isset($args[0])) {
- 			if($sender->hasPermission('napvip.command')){
- 				return true;
- 			}
- 			else{
- 				$sender->sendMessage(TextFormat::RED."You do not have permission to use this command");
- 				return true;
- 			}
- 		}
- 		switch (strtolower($args[0])){
- 			case'isset($args[1])':
- 				if($this->mang($args[1])){
- 						$this->activeMode($sender, $args(1));
- 						
- 					}else{
- 						$sender->sendMessage(TextFormat::BLUE . "=====Help===");
- 						$sender->sendMessage(TextFormat::GOLD . "/napvip <mobi|vittel|vina>");
- 						$sender->sendMessage(TextFormat::GOLD . "/napxu <mobi|vittel|vina>");
- 						$sender->sendMessage(TextFormat::BLUE . "/card top");
- 					}
- 				
- 							
- 				
 
- 		}
-	}
 }
